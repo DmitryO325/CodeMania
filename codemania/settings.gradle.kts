d04359452744bf5a7e2f1adf1d74237e1936747a
Dimon <oberemokdima05@gmail.com>
2026-07-01 14:43:02 +0300

    Структура проекта

---
+++ codemania/settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "Codemania"
+
+include(":app")
+
+include(":core:common")
+include(":core:network")
+include(":core:database")
+include(":core:ui")
+include(":core:designsystem")
+
+include(":domain")
+include(":data")
+
+include(":feature:auth")
+include(":feature:catalog")
+include(":feature:cart")
+include(":feature:course")
+include(":feature:lesson")
+include(":feature:schedule")
+include(":feature:chat")
+include(":feature:profile")
